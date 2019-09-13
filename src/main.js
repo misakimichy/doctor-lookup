80dfad8e07e05afab5c9f976b2de09081878a351
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-13 10:52:40 -0700

    Add basic contents.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 // Frontend logic
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    $("#result").show();
+  });
+});
