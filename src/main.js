b4ac77aad6dbc680596fb96f88e2149f1427895e
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-13 12:45:37 -0700

    Call getDoctorBySymptom method from main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { parseData } from './parse';
+import { DoctorSearch } from './api';
 
-// Frontend logic
+// Call APi and show the proper result to user
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    $("#result").show();
+
+    const userInput = $("#medical-concern").val();
+    const doctorSearch = new DoctorSearch();
+    const promise = doctorSearch.getDoctorBySymptom(userInput);
+
+    promise.then(result => {
+      const output = JSON.parse(result);
+      parseData(output);
+    }).catch(error => {
+      console.log(error);
+    })
+
+    // $("#result").show();
   });
 });
