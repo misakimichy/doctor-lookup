50814dc991b8577e28f78b2595ced0ead99048da
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-15 11:45:49 -0700

    Add condition to render information only if the doctor's office is in Seattle.

--- src/parse.js
+++ src/parse.js
@@ -20,30 +20,35 @@ export class Data {
     if(doctorList.length == 0) {
       throw new Error ("Sorry, your search returned no results.");
     } else {
+      // Function to append result to html
+      const renderInfo = (text) => {
+        return $("#doctor-info").append(text);
+      };
       // Doctor's name, specialty and biography
       for(let i = 0; i < doctorList.length; i++) {
-        this.doctorName = `${doctorList[i].profile.first_name} ${doctorList[i].profile.middle_name} ${doctorList[i].profile.last_name} ${doctorList[i].profile.title}`;
-        $("#doctor-info").append(`<p id="doctor-name">${this.doctorName}</p>`);
-        this.specialty = doctorList[i].specialties[0].actor;
-        $("#doctor-info").append(`<p>Specialty: ${this.specialty}</p>`);
-        this.bio = doctorList[i].profile.bio;
-        $("#doctor-info").append(`<p>Biography: ${this.bio}</p>`);
-        
-        // Doctor's office info
-        for(let j = 0; j < (doctorList[i].practices).length; j++){
-          const listPractice = doctorList[i].practices[j];
-          this.acceptNew = listPractice.accepts_new_patients; // boolean
-          this.acceptNew ? $("#doctor-info").append(`<p>Accept New Patients: Yes</p>`) : $("#doctor-info").append(`<p>Accept New Patients: No</p>`);
-
-          this.officeName = listPractice.name;
-          this.website = listPractice.website;
-          $("#doctor-info").append(`<p>Website: <a href=${this.website}>${this.officeName}</a></p>`);
-
-          this.officeAddress = `${listPractice.visit_address.street}, ${listPractice.visit_address.city}, ${listPractice.visit_address.state} ${listPractice.visit_address.zip}`;
-          this.phoneNumber = listPractice.phones[0].number;
-          $("#doctor-info").append(`<p>Address: ${this.officeAddress}</p>`);
-          $("#doctor-info").append(`<p>TEL: ${this.phoneNumber}</p><hr>`);
-          break;
+        // Render info only if the doctor's office is in Seattle
+        if(doctorList[i].practices[0].visit_address.city.includes("Seattle") === true) {
+          this.doctorName = `${doctorList[i].profile.first_name} ${doctorList[i].profile.middle_name} ${doctorList[i].profile.last_name} ${doctorList[i].profile.title}`;
+          this.specialty = doctorList[i].specialties[0].actor;
+          this.bio = doctorList[i].profile.bio;
+          renderInfo(`<p id="doctor-name">${this.doctorName}</p>`);
+          renderInfo(`<p>Specialty: ${this.specialty}</p>`);
+          renderInfo(`<p>Biography: ${this.bio}</p>`);
+          
+          // Doctor's office info
+          for(let j = 0; j < (doctorList[i].practices).length; j++){
+            const listPractice = doctorList[i].practices[j];
+            this.acceptNew = listPractice.accepts_new_patients; // boolean
+            this.officeName = listPractice.name;
+            this.website = listPractice.website;
+            this.officeAddress = `${listPractice.visit_address.street}, ${listPractice.visit_address.city}, ${listPractice.visit_address.state} ${listPractice.visit_address.zip}`;
+            this.phoneNumber = listPractice.phones[0].number;
+            this.acceptNew ? renderInfo(`<p>Accept New Patients: Yes</p>`) : renderInfo(`<p>Accept New Patients: No</p>`);
+            renderInfo(`<p>Website: <a href=${this.website}>${this.officeName}</a></p>`);
+            renderInfo(`<p>Address: ${this.officeAddress}</p>`);
+            renderInfo(`<p>TEL: ${this.phoneNumber}</p><hr>`);
+            break;
+          }
         }
       }
     }  
