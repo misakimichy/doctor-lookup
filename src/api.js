f0215060a2bc75cf0dfc52ee2f4919a5ddbdfcfd
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-15 12:36:11 -0700

    Create API call for search by Docto name

--- src/api.js
+++ src/api.js
@@ -16,4 +16,21 @@ export class DoctorSearch {
       request.send();
     });
   }
+
+  // method to call doctors list by Doctor's name
+  searchDoctorByName(name) {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=wa-seattle&skip=0&limit=10&user_key=${process.env.apiKey}`;
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
